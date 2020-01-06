3e55cf49b26517a9bbf908bccd4d23dfa71605f5
root <root@ik1-306-13474.vs.sakura.ne.jp>
2020-01-06 21:47:05 +0900

    a

--- optqueue/settings/__init__.py
+++ optqueue/settings/__init__.py
@@ -1,6 +1,9 @@
 import os
 
-if 'env' in os.environ and os.environ['env'] == 'prod':
+k = 'queue_env'
+print('queue_env' in os.environ)
+print(os.environ)
+if 'queue_env' in os.environ and os.environ['queue_env'] == 'prod':
    from .prod import *
 else:
    from .dev import *
